f652ab4ee61aa783abfb6a51becbd13044140a9d
Luan-Amor <37778171+Luan-Amor@users.noreply.github.com>
2018-11-27 08:54:59 -0200

    Add files via upload

---
+++ Jogos/Pula/Sprite.js
@@ -0,0 +1,15 @@
+function sprite(x, y, largura, altura){
+    this.x = x;
+    this.y = y;
+    this.largura = largura;
+    this.altura = altura;
+
+    this.desenha = function(xCanvas, yCanvas){
+        ctx.drawImage(img, this.x, this.y, this.largura, this.altura, xCanvas, yCanvas, this.largura, this.altura);
+    }
+}
+
+
+
+var bg = new sprite(0, 0, 600, 600),
+spriteBoneco = new sprite(618, 16, 50, 50);
